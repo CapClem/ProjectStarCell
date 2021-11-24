7fbd7740ed3b56515bbff116e0c7448ca8f55936
1018360 <1018360@student.sae.edu.au>
2021-11-24 13:49:58 +1100

    made automatic door

--- Assets/Team Members/Tim/Scripts/Door.cs
+++ Assets/Team Members/Tim/Scripts/Door.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool locked;
+    private Vector3 startPos;
+
+    private void Start()
     {
-        
+        startPos = transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (locked == false)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Open());
+            }
+        }
+
+        if (locked == true)
+        {
+            GetComponent<Collider>().isTrigger = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (locked == false)
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+    }
+
+    IEnumerator Open()
+    {
+        transform.position = startPos + new Vector3(0, 2, 0);
+        yield return new WaitForSeconds(.5f);
+        transform.position = startPos;
     }
 }
